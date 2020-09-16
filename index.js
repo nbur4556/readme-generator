5313c1291697d4bb93ac2d048665d5d5da9447aa
nbur4556 <nbur4556@gmail.com>
2020-09-16 17:58:57 -0500

    created markdown file template

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const inquirer = require('inquirer');
 
-ask();
+getUserInput()
+console.log(buildMarkdownFile());
 
-async function ask() {
-    console.log(await promptUser('Hello'));
-    console.log(await promptUser('How are you?'));
+async function getUserInput() {
+    const userInput = {
+        title: '',
+        description: '',
+        installation: '',
+        usage: '',
+        license: '',
+        contributors: '',
+        tests: '',
+        questions: ''
+    }
+
+    userInput.title = await promptUser('What is the project title?');
+    userInput.description = await promptUser('What is the project description');
+    userInput.installation = await promptUser('What are the installation instructions for the project?');
+    userInput.usage = await promptUser('What are the usage instructions for the project?');
+    userInput.license = await promptUser('What license does the project use?');
+    userInput.contributors = await promptUser('Who are the contributors of the project?');
+    userInput.tests = await promptUser('What tests were used on the project');
+    userInput.questions = await promptUser('What is the contact information for the project?');
+
+    console.log(userInput);
 }
 
 function promptUser(msg) {
@@ -19,4 +39,42 @@ function promptUser(msg) {
             reject(err);
         });
     })
+}
+
+function buildMarkdownFile() {
+    return (`
+        # TITLE
+        
+        ## Description
+
+        DESCRIPTION
+
+        ## Table of Contents
+
+        TABLE OF CONTENTS
+
+        ## Installation
+
+        INSTALLATION
+
+        ## Usage
+
+        USAGE
+
+        ## License
+
+        LICENSE
+
+        ## Contributors
+
+        CONTRIBUTORS
+
+        ## Tests
+
+        TESTS
+
+        ## Questions
+
+        QUESTIONS
+    `)
 }
