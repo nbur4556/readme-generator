2f4112668454df43937b22d21130af56c9f1eab3
nbur4556 <nbur4556@gmail.com>
2020-09-16 17:20:38 -0500

    gets input from prompts asynchronously

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const inquirer = require('inquirer');
 
-inquirer.prompt({
-    message: 'Enter any value',
-    name: 'promptValue',
-    type: 'input'
-}).then(answer => {
-    console.log(answer.promptValue);
-})
+getUserInput();
+
+async function getUserInput() {
+    await inquirer.prompt({
+        message: 'Enter any value',
+        name: 'promptValue',
+        type: 'input'
+    }).then(answer => {
+        console.log(answer.promptValue);
+    })
+
+    await inquirer.prompt({
+        message: 'Enter new value',
+        name: 'promptValue',
+        type: 'input'
+    }).then(answer => {
+        console.log(answer.promptValue);
+    })
+}
