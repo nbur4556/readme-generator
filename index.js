af9c5db1008f889aa98d0b4ac46caf256613d9bc
nbur4556 <nbur4556@gmail.com>
2020-09-18 18:38:21 -0500

    included new license objects as array for license list options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const readme = require('./src/build-readme.js');
+const licenses = require('./src/license-options.js');
 
 const userData = {
     title: '',
@@ -12,17 +13,6 @@ const userData = {
     tests: '',
     questions: ''
 };
-
-const licenseOptions = [
-    'Academic Free License v3.0', 'Apache license 2.0', 'Artistic license 2.0', 'Boost Software License 1.0', 'BSD 2-clause "Simplified" license',
-    'BSD 3-clause "New" or "Revised" license', 'BSD 3-clause Clear license', 'Creative Commons license family', 'Creative Commons Zero v1.0 Universal',
-    'Creative Commons Attribution 4.0', 'Creative Commons Attribution Share Alike 4.0', 'Do What The F*ck You Want To Public License',
-    'Educational Community License v2.0', 'Eclipse Public License 1.0', 'European Union Public License 1.1', 'GNU Affero General Public License v3.0',
-    'GNU General Public License family', 'GNU General Public License v2.0', 'GNU General Public License v3.0', 'GNU Lesser General Public License family',
-    'GNU Lesser General Public License v2.1', 'GNU Lesser General Public License v3.0', 'ISC', 'LaTeX Project Public License v1.3c',
-    'Microsoft Public License', 'MIT', 'Mozilla Public License 2.0', 'Open Software License 3.0', 'PostgreSQL License', 'SIL Open Font License 1.1',
-    'University of Illinois/NCSA Open Source License', 'The Unlicense', 'zLib License'
-]
 
 // Runtime
 getUserInput()
@@ -36,7 +26,7 @@ async function getUserInput() {
     userData.description = await promptUser('What is the project description');
     userData.installation = await promptUser('What are the installation instructions for the project?');
     userData.usage = await promptUser('What are the usage instructions for the project?');
-    userData.license = await promptUserList('What license does the project use?', licenseOptions);
+    userData.license = await promptUserList('What license does the project use?', licenses);
     userData.contributors = await promptUser('Who are the contributors of the project?');
     userData.tests = await promptUser('What tests were used on the project');
     userData.questions = await promptUser('What is the contact information for the project?');
